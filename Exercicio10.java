caa14a9eea07fd97b7758fe6561b074137731d60
Milena da Silva Fulaneto <132371316+MilenaFurlaneto@users.noreply.github.com>
2023-05-03 10:31:34 -0300

    Update Exercicio10.java

--- Exercicio10.java
+++ Exercicio10.java
@@ -1,5 +1,24 @@
-package Atividade240523;
+package activity25042023;
 
-public class Exercicio10 {
+ import java.util.Scanner;
+
+public class exercicio10 {
+	
+	public static void main (String[]args) {
+	 Scanner s = new Scanner(System.in);
+	 
+	 double n1;
+	 
+	 System.out.println("Digite um numero:");
+	 n1 = s.nextDouble();
+	 
+	 for(int i = 1; i <= 1000;i++) {
+		 if (((n1 % i) ==0)) {
+			  System.out.println("O numero" + n1 + "e Divisivel por:"+ i);
+			  
+		 }
+	 }
+	}
 
 }
+
