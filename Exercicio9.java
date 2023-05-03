79ba8518968bc946599d929d17fd1de0f0c1470a
Milena da Silva Fulaneto <132371316+MilenaFurlaneto@users.noreply.github.com>
2023-05-03 10:32:37 -0300

    Update Exercicio9.java

--- Exercicio9.java
+++ Exercicio9.java
@@ -1,5 +1,29 @@
-package Atividade240523;
+package activity25042023;
+ 
+ import java.util.Scanner;
 
-public class Exercicio9 {
+public class exercicio9 {
+	
+	public static void main (String[] angs){
+	
+	 Scanner s = new Scanner (System.in);
+	 
+	     double idade;
+	     String nome;
+	     
+	     System.out.println ("Escreve seu nome:");
+	     nome = s.next();
+	     
+	     System.out.println ("Escreva sua idade:");
+	     idade = s.nextDouble ();
+	     
+	     if (idade >18) {
+	    	 
+	    	    System.out.println ("Voce e maior de idade");
+	     }else {
+	    	 System.out.println("Voce e menor de idade");
+	    	
+	     }
+	}
 
 }
